488e8be662daf9e3e6e4251ab164fd58838d2aee
sevevaldez <cvvaldez00@gmail.com>
2018-09-16 00:20:54 -0500

    Add functionality for tabs

--- app/src/main/java/com/dacode/prueba/venadostest/activities/MainActivity.kt
+++ app/src/main/java/com/dacode/prueba/venadostest/activities/MainActivity.kt
@@ -3,12 +3,16 @@ package com.dacode.prueba.venadostest.activities
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.design.widget.TabLayout
 import android.support.v4.view.GravityCompat
+import android.support.v4.view.ViewPager
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.dacode.prueba.venadostest.R
+import com.dacode.prueba.venadostest.adapters.PageAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -18,6 +22,34 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        val tabLayout : TabLayout = findViewById(R.id.tabLayout)
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.actmain_tab_name_copa)))
+        tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.actmain_tab_name_ascenso)))
+
+        val viewPager: ViewPager = findViewById(R.id.viewPager)
+        val pagerAdapter = PageAdapter(supportFragmentManager, tabLayout.tabCount)
+
+        viewPager.adapter = pagerAdapter
+
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Reselected", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Unselected", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val position = tab!!.position
+                viewPager.currentItem = position
+            }
+
+        })
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
