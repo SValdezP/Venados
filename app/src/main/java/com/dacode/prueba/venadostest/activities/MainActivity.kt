576577ba33616660a625281ee72a4d49914356e3
sevevaldez <cvvaldez00@gmail.com>
2018-09-15 20:41:27 -0500

    Add fragments

--- app/src/main/java/com/dacode/prueba/venadostest/activities/MainActivity.kt
+++ app/src/main/java/com/dacode/prueba/venadostest/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dacode.prueba.venadostest
+package com.dacode.prueba.venadostest.activities
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.dacode.prueba.venadostest.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
